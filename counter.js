cb45cd5920317e418f19f3ae44b4522af4c769a0
Jimmyst <08jimmy91@gmail.com>
2024-07-07 18:47:00 -0400

    dynamic generation

--- counter.js
+++ counter.js
@@ -1,112 +1,133 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
-import { getDatabase,  ref, child, get } from "firebase/database";
-import { getAuth,signInWithEmailAndPassword, createUserWithEmailAndPassword  } from 'firebase/auth';
-
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getDatabase, ref, child, get } from "firebase/database";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const realtime_database = getDatabase(app);
-const auth = getAuth(app)
-let globalCounter = 0
+const auth = getAuth(app);
+let globalCounter = 0;
 // const analytics = getAnalytics(app);
 const querySnapshot = await getDocs(collection(db, "/test"));
-  querySnapshot.forEach((doc) => {
-    globalCounter = doc.data().value
-    console.log(`${doc.id} => ${doc.data().value}`);
-  });
+querySnapshot.forEach((doc) => {
+  globalCounter = doc.data().value;
+  console.log(`${doc.id} => ${doc.data().value}`);
+});
 
-
-  // const starCountRef = ref(realtime_database, '/products');
-  // onValue(starCountRef, (snapshot) => {
-  //   const data = snapshot.val();
-  //   console.log(data)
-  // });
-
-  const dbRef = ref(realtime_database);
-  get(child(dbRef, `/products`)).then((snapshot) => {
+// const starCountRef = ref(realtime_database, '/products');
+// onValue(starCountRef, (snapshot) => {
+//   const data = snapshot.val();
+//   console.log(data)
+// });
+let myProducts = {};
+const dbRef = ref(realtime_database);
+get(child(dbRef, `/products`))
+  .then((snapshot) => {
     if (snapshot.exists()) {
       console.log(snapshot.val());
+      myProducts = snapshot.val();
+      generateDivs();
     } else {
       console.log("No data available");
     }
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.error(error);
   });
 
-
 export function setupCounter(element) {
-  
-  let counter = globalCounter
-  
+  let counter = globalCounter;
+
   const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(counter)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(counter);
 }
 
-
 //авторизация
-const authForm = document.getElementById('auth-form');
-        const emailInput = document.getElementById('email');
-        const passwordInput = document.getElementById('password');
-        const signInButton = document.getElementById('sign-in');
-        const signUpButton = document.getElementById('sign-up');
-        const errorMessage = document.getElementById('error-message');
+const authForm = document.getElementById("auth-form");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const signInButton = document.getElementById("sign-in");
+const signUpButton = document.getElementById("sign-up");
+const errorMessage = document.getElementById("error-message");
 
+authForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const email = emailInput.value;
+  const password = passwordInput.value;
 
-        authForm.addEventListener('submit', function(event) {
-          event.preventDefault();
-          const email = emailInput.value;
-          const password = passwordInput.value;
+  if (signInButton === event.submitter) {
+    SignIn(email, password);
+  } else if (signUpButton === event.submitter) {
+    SignUp(email, password);
+  }
+});
 
-          if (signInButton === event.submitter) {
-              SignIn(email, password);
-          } else if (signUpButton === event.submitter) {
-              SignUp(email, password);
-          }
-      });
+// Sign in with email and password
+function SignIn(email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user.uid); // Redirect to signed-in page
+    })
+    .catch((error) => {
+      errorMessage.textContent = error.message;
+    });
+}
 
-      // Sign in with email and password
-      function SignIn(email, password) {
-        signInWithEmailAndPassword(auth,email, password)
-              .then((userCredential) => {
-                  // Signed in
-                  const user = userCredential.user;
-                  console.log(user.uid) // Redirect to signed-in page
-              })
-              .catch((error) => {
-                  errorMessage.textContent = error.message;
-              });
-      }
+// Sign up with email and password
+function SignUp(email, password) {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up and signed in
+      const user = userCredential.user;
+      console.log(user.uid); // Redirect to signed-in page
+    })
+    .catch((error) => {
+      errorMessage.textContent = error.message;
+    });
+}
 
-      // Sign up with email and password
-      function SignUp(email, password) {
-        createUserWithEmailAndPassword(auth,email, password)
-              .then((userCredential) => {
-                  // Signed up and signed in
-                  const user = userCredential.user;
-                  console.log(user.uid); // Redirect to signed-in page
-              })
-              .catch((error) => {
-                  errorMessage.textContent = error.message;
-              });
-      }
+//динамические обхекты
+
+function generateDivs() {
+  const container = document.getElementById("my_products");
+  for (const [key, value] of Object.entries(myProducts)) {
+    console.log(key, value);
+    const button = document.createElement("button");
+    button.className = "button_" + key;
+    button.textContent = "Продукт базы: " + key;
+    button.addEventListener("click", function (btn) {
+      console.log("Нажат продукт " + this.className);
+    });
+    container.appendChild(button);
+
+  }
+}
+
+// generateDivs()
